0725b63a12fb78745e8f39be750faf8f92123d43
Viko Feta <skripka0807@gmail.com>
2023-05-09 18:01:57 +0300

    start

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>HELLO</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
